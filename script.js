11231ce48b0244494f47ced821e1701d5a750b82
kumarAjay10 <144784190+kumarAjay10@users.noreply.github.com>
2023-12-01 10:52:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let count = document.getElementById("counter");
+let incrementBtn = document.getElementById("incrementBtn");
+
+ let counterValue = 0;
+ count.textContent = counterValue;
+funtion myFun(){
+	alert( "un-incremented value: " + counterValue );
+	counterValue++;
+	count.textContent = counterValue;
+}
+incrementBtn.addEventListener("click",myFun);
